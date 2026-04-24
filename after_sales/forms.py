064eb3476751e0e71229d538475d57b92f3d2d69
Palak <Kamil.Palak@sharpconsumer.eu>
2026-04-24 14:54:16 +0200

    dodano if settings.DEBUG w urls.py - aktywowanie css

--- after_sales/forms.py
+++ after_sales/forms.py
@@ -8,4 +8,7 @@ class ServiceTicketForm(forms.ModelForm):
         fields = ['ticket_number', 'customer', 'purchase_date', 'serial_number', 'is_warranty', 'device_model']
         widgets = {
             'purchase_date': forms.DateInput(attrs={'type': 'date'}),
+            'customer': forms.Select(attrs={'class': 'input-standard'}),
+            'device_model': forms.Select(attrs={'class': 'input-standard'}),
+            'serial_number': forms.TextInput(attrs={'class': 'input-standard', 'placeholder': 'Wpisz numer SN...'}),
         }
