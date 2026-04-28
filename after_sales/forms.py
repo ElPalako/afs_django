6e275da332f3e62af5e9c3d1b44a1dddafcbfa57
Palak <Kamil.Palak@sharpconsumer.eu>
2026-04-28 14:37:38 +0200

    Dodanie description do formularza, porpawienie css

--- after_sales/forms.py
+++ after_sales/forms.py
@@ -5,11 +5,12 @@ class ServiceTicketForm(forms.ModelForm):
     class Meta:
         model = ServiceTicket
         # Wybieramy, które pola z bazy chcemy pokazać pracownikowi
-        fields = ['ticket_number', 'customer', 'purchase_date', 'serial_number', 'is_warranty', 'device_model']
+        fields = ['ticket_number', 'customer', 'purchase_date', 'serial_number', 'is_warranty', 'device_model', 'description']
         widgets = {
             'ticket_number': forms.TextInput(attrs={'class': 'input-standard', 'placeholder': 'Wpisz numer RMA...'}),
             'purchase_date': forms.DateInput(attrs={'type': 'date'}),
             'customer': forms.Select(attrs={'class': 'input-standard'}),
             'device_model': forms.Select(attrs={'class': 'input-standard'}),
             'serial_number': forms.TextInput(attrs={'class': 'input-standard', 'placeholder': 'Wpisz numer SN...'}),
+            'description': forms.Textarea(attrs={'class': 'textarea-standard', 'placeholder': 'Wprowadź opis wady...', 'rows': 4}),
         }
